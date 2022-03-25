264c293bc04ff2970834c1ab1c7e3b370404ebad
Lancy <lf@mega.co.nz>
2022-03-25 13:17:21 +1300

    excluding the license and podspec file.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDAVAssetExportSession",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["LICENSE", "SDAVAssetExportSession.podspec"]
+        ),
         .testTarget(
             name: "SDAVAssetExportSessionTests",
             dependencies: ["SDAVAssetExportSession"]),
